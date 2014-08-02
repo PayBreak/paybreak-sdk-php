d309f0217624e5c5171227d875a5d17abfed6c49
wnowicki <wonowicki@gmail.com>
2014-08-02 14:08:29 +0100

    Added Entity Standard Interface

---
+++ lib/StandardInterface/EntityInterface.php
@@ -0,0 +1,26 @@
+<?php
+/*
+ * This file is part of the Graham package.
+ *
+ * (c) Wojciech Nowicki <wojtek@gettelegramm.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Graham\StandardInterface;
+
+/**
+ * Standard Interface EntityInterface
+ *
+ * @package Graham\StandardInterface
+ */
+interface EntityInterface
+{
+    /**
+     * Returns entity as array
+     *
+     * @return array
+     */
+    public function toArray();
+}
