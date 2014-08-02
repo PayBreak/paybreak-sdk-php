3632c087d2dd4f8996e29cf6c2e30e866e251ba0
wnowicki <wonowicki@gmail.com>
2014-08-02 15:34:09 +0100

    Added Simple Loan Request

---
+++ lib/LoanRequest/Entity/SimpleLoanRequest.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ * This file is part of the Graham package.
+ *
+ * (c) Wojciech Nowicki <wojtek@gettelegramm.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Graham\LoanRequest\Entity;
+
+/**
+ * Class SimpleLoanRequest
+ *
+ * @package Graham\LoanRequest\Entity
+ */
+class SimpleLoanRequest extends LoanRequestAbstract
+{
+    /**
+     * Returns Checkout Type
+     *
+     * @return int
+     */
+    public function getCheckoutType()
+    {
+        return self::TYPE_SIMPLE;
+    }
+
+}
