9eaaae7776f84a5bc3ac6d0ab780a14f65bd41a4
wnowicki <wonowicki@gmail.com>
2014-08-16 23:04:30 +0100

    Setup PHPUnit conf

---
+++ test/TestCase.php
@@ -0,0 +1,22 @@
+<?php
+/*
+ * This file is part of the Graham package.
+ *
+ * (c) Wojciech Nowicki <wojtek@gettelegramm.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Graham\Tests;
+
+/**
+ * Class TestCase - Default configuration for this test case
+ *
+ * @author WN
+ * @package Graham\Tests
+ */
+abstract class TestCase extends \PHPUnit_Framework_TestCase
+{
+
+}
---
+++ test/bootstrap.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * This file is part of the Graham package.
+ *
+ * (c) Wojciech Nowicki <wojtek@gettelegramm.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+$loader = include __DIR__ . "/../vendor/autoload.php";
+$loader->addPsr4('Graham\\Tests\\', __DIR__);
