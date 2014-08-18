d96097fe83c5dc362a5640c42d0ca7445045cbf0
WNowicki <wojciech.nowicki@paybreak.com>
2014-08-18 16:16:46 +0100

    Copyright change

--- lib/CustomType/RequestCustomer.php
+++ lib/CustomType/RequestCustomer.php
@@ -2,7 +2,7 @@
 /*
  * This file is part of the PayBreak\Sdk package.
  *
- * (c) Wojciech Nowicki <wojtek@gettelegramm.org>
+ * (c) PayBreak <dev@paybreak.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
