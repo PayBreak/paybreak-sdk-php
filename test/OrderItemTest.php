2e3c0d6674e905a311eebbb3148332e3ad1146b0
mattpaybreak <matthew.norris@paybreak.com>
2014-11-25 14:04:58 +0000

    Added order item unit test

---
+++ test/OrderItemTest.php
@@ -0,0 +1,109 @@
+<?php
+
+/**
+ * Unit test for the OrderItem class
+ * @author Matthew Norris
+ */
+
+namespace PayBreak\Sdk\Tests;
+
+class OrderItemTest extends TestCase {
+
+    private $orderItem;
+
+    const SKU = "SKU_01234";
+    const GTIN = "01234";
+    const DESCRIPTION = "This is an order item description";
+    const PRICE = 9000;
+    const QUANTITY = 10;
+    const FULFILLABLE = true;
+    const FULFILLED = 9000; // this is an amount in pence. Not actually used by the SDK.
+
+    protected function setUp()
+    {
+        $this->orderItem = new \PayBreak\Sdk\CustomType\OrderItem();
+    }
+
+    protected function tearDown()
+    {
+        $this->orderItem = null;
+    }
+
+    public function testSetSku()
+    {
+        $this->assertSame(self::SKU, $this->orderItem->setSku(self::SKU));
+    }
+
+    public function testGetSku()
+    {
+        $this->orderItem->setSku(self::SKU);
+        $this->assertSame(self::SKU, $this->orderItem->getSku());
+    }
+
+    public function testSetGtin()
+    {
+        $this->assertSame(self::GTIN, $this->orderItem->setGtin(self::GTIN));
+    }
+
+    public function testGetGtin()
+    {
+        $this->orderItem->setGtin(self::GTIN);
+        $this->assertSame(self::GTIN, $this->orderItem->getGtin());
+    }
+
+    public function testSetDescription()
+    {
+        $this->assertSame(self::DESCRIPTION, $this->orderItem->setDescription(self::DESCRIPTION));
+    }
+
+    public function testGetDescription()
+    {
+        $this->orderItem->setDescription(self::DESCRIPTION);
+        $this->assertSame(self::DESCRIPTION, $this->orderItem->getDescription());
+    }
+
+    public function testSetPrice()
+    {
+        $this->assertSame(self::PRICE, $this->orderItem->setPrice(self::PRICE));
+    }
+
+    public function testGetPrice()
+    {
+        $this->orderItem->setPrice(self::PRICE);
+        $this->assertSame(self::PRICE, $this->orderItem->getPrice());
+    }
+
+    public function testSetQuantity()
+    {
+        $this->assertSame(self::QUANTITY, $this->orderItem->setQuantity(self::QUANTITY));
+    }
+
+    public function testGetQuantity()
+    {
+        $this->orderItem->setQuantity(self::QUANTITY);
+        $this->assertSame(self::QUANTITY, $this->orderItem->getQuantity());
+    }
+
+    public function testSetFulfillable()
+    {
+        $this->assertSame(self::FULFILLABLE, $this->orderItem->setFulfillable(self::FULFILLABLE));
+    }
+
+    public function testGetFulfillable()
+    {
+        $this->orderItem->setFulfillable(self::FULFILLABLE);
+        $this->assertSame(self::FULFILLABLE, $this->orderItem->getFulfillable());
+    }
+
+    public function testSetFulfilled()
+    {
+        $this->assertSame(self::FULFILLED, $this->orderItem->setFulfilled(self::FULFILLED));
+    }
+
+    public function testGetFulfilled()
+    {
+        $this->orderItem->setFulfilled(self::FULFILLED);
+        $this->assertSame(self::FULFILLED, $this->orderItem->getFulfilled());
+    }
+
+} 
