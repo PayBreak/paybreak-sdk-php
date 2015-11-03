0fc47ca03797363564ca010af17880256c80c9e0
EvanBarbour3 <EvanBarbour@hotmail.co.uk>
2015-11-03 12:31:58 +0000

    Group entity changes

--- src/Entities/GroupEntity.php
+++ src/Entities/GroupEntity.php
@@ -13,22 +13,84 @@ namespace PayBreak\Sdk\Entities;
 use WNowicki\Generic\AbstractEntity;
 
 /**
- * Application Entity
+ * Group Entity
  *
  * @author EB
  * @method $this setId(int $id)
  * @method int|null getId()
  * @method $this setName(string $name)
  * @method string|null getName()
- * @method $this setProducts(ProductEntity $products)
- * @method ProductEntity|null getProducts()
  * @package PayBreak\Sdk\Entities
  */
 class GroupEntity extends AbstractEntity
 {
+    private $products = [];
+
     protected $properties = [
         'id' => self::TYPE_STRING,
         'name' => self::TYPE_STRING,
-        'products' => self::TYPE_ARRAY,
     ];
+
+    /**
+     * @return ProductEntity[]
+     */
+    public function getProducts()
+    {
+        return $this->products;
+    }
+
+    /**
+     * @param ProductEntity $product
+     */
+    public function addProduct(ProductEntity $product)
+    {
+        $this->products[] = $product;
+    }
+
+    /**
+     * Make Entity
+     *
+     * @author WN
+     * @param array $components
+     * @return static
+     */
+    public static function make(array $components)
+    {
+        $entity = parent::make($components);
+
+        if (array_key_exists('products', $components)) {
+            foreach ($components['products'] as $product) {
+                if (is_array($product)) {
+                    $entity->addProduct(ProductEntity::make($product));
+                    continue;
+                }
+                if ($product instanceof ProductEntity) {
+                    $entity->addProduct($product);
+                }
+            }
+        }
+
+        return $entity;
+    }
+
+    /**
+     * To Array
+     *
+     * @author WN
+     * @param bool $recursively If set to `true` then toArray(true) will be called on each `Arrayable` property
+     * @return array
+     */
+    public function toArray($recursively = false)
+    {
+        $ar =  parent::toArray($recursively);
+
+        if ($recursively) {
+            foreach ($this->getProducts() as $product) {
+
+                $ar['products'][] = $product->toArray();
+            }
+        }
+
+        return $ar;
+    }
 }
