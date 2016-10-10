529effe5d37f54be1aadfebae5d90b3e4e610322
Evan Barbour <EvanBarbour@hotmail.co.uk>
2016-10-10 10:47:18 +0100

    Proper doc block changes

--- src/Entities/InstallationEntity.php
+++ src/Entities/InstallationEntity.php
@@ -17,26 +17,28 @@ use WNowicki\Generic\AbstractEntity;
  * Installation Entity
  *
  * @author WN
- * @method $this setId(string $id)
+ * @method $this setId($id)
  * @method string|null getId()
- * @method $this setName(string $id)
+ * @method $this setName($name)
  * @method string|null getName()
- * @method $this setReturnUrl(string $id)
+ * @method $this setReturnUrl($returnUrl)
  * @method string|null getReturnUrl()
- * @method $this setNotificationUrl(string $id)
+ * @method $this setNotificationUrl($notificationUrl)
  * @method string|null getNotificationUrl()
- * @method $this setDefaultProduct(string $id)
+ * @method $this setDefaultProduct($defaultProduct)
  * @method string|null getDefaultProduct()
+ * @method $this setFeatures(FeaturesEntity $features)
+ * @method FeaturesEntity|null getFeatures()
  * @package PayBreak\Sdk\Entities
  */
 class InstallationEntity extends AbstractEntity
 {
     protected $properties = [
-         'id',
-         'name',
-         'return_url',
-         'notification_url',
-         'default_product',
+        'id',
+        'name',
+        'return_url',
+        'notification_url',
+        'default_product',
         'features' => FeaturesEntity::class,
     ];
 }
