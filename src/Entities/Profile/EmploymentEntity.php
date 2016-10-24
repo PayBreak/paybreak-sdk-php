a5403fae7ec634b98f60508b2f3eb5f83d546f89
Manny <emmanuel.adeyemi@paybreak.com>
2016-10-24 14:32:05 +0100

    Employment Entity

---
+++ src/Entities/Profile/EmploymentEntity.php
@@ -0,0 +1,37 @@
+<?php
+/*
+ * This file is part of the PayBreak/basket package.
+ *
+ * (c) PayBreak <dev@paybreak.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace PayBreak\Sdk\Entities\Profile;
+
+use WNowicki\Generic\AbstractEntity;
+
+/**
+ * Employment Entity
+ *
+ * @author EA
+ * @method $this setUser(int $user)
+ * @method int|null getUser()
+ * @method $this setEmploymentStatus(int $employmentStatus)
+ * @method int|null getEmploymentStatus()
+ * @method $this setEmploymentStart(string $employmentStart)
+ * @method string|null getEmploymentStart()
+ * @method $this setEmploymentPhone(string $employmentPhone)
+ * @method string|null getEmploymentPhone()
+ *
+ * @package PayBreak\Sdk\Entities\Profile;
+ */
+class EmploymentEntity extends AbstractEntity
+{
+    protected $properties = [
+        'employment_status' => self::TYPE_INT,
+        'employment_start' => self::TYPE_STRING,
+        'phone_employer' => self::TYPE_STRING,
+    ];
+}
