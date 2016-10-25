d85f2a614804880016b55c38e11bba9026906783
Evan Barbour <EvanBarbour@hotmail.co.uk>
2016-10-25 15:16:38 +0100

    Proper extension of base class

--- src/Entities/AssistedApplicationEntity.php
+++ src/Entities/AssistedApplicationEntity.php
@@ -13,15 +13,28 @@ namespace PayBreak\Sdk\Entities;
 /**
  * Assisted Application Entity
  *
- * @author WN
+ * @author EA, EB, SL
  * @method $this setEmail(string $email)
  * @method string|null getEmail()
  * @package PayBreak\Sdk\Entities
  */
 class AssistedApplicationEntity extends ApplicationEntity
 {
-    protected $properties =
-        [
-            'email' => 'email',
-        ];
+    protected $properties = [
+        'id',
+        'posted_date',
+        'current_status',
+        'customer' => 'PayBreak\Sdk\Entities\Application\CustomerEntity',
+        'application_address' => 'PayBreak\Sdk\Entities\Application\AddressEntity',
+        'installation',
+        'order' => 'PayBreak\Sdk\Entities\Application\OrderEntity',
+        'products' => 'PayBreak\Sdk\Entities\Application\ProductsEntity',
+        'fulfilment' => 'PayBreak\Sdk\Entities\Application\FulfilmentEntity',
+        'applicant' => 'PayBreak\Sdk\Entities\Application\ApplicantEntity',
+        'finance' => 'PayBreak\Sdk\Entities\Application\FinanceEntity',
+        'cancellation' => 'PayBreak\Sdk\Entities\Application\CancellationEntity',
+        'metadata',
+        'resume_url',
+        'email' => self::TYPE_STRING,
+    ];
 }
