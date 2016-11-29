1ffabff77e273bc8558cc82d343cda18d8a8b257
Manny <emmanuel.adeyemi@paybreak.com>
2016-11-29 11:56:02 +0000

    Settlement Csv Gateway

---
+++ src/Gateways/SettlementCsvGateway.php
@@ -0,0 +1,22 @@
+<?php
+/*
+* This file is part of the PayBreak/basket package.
+*
+* (c) PayBreak <dev@paybreak.com>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+namespace PayBreak\Sdk\Gateways;
+
+/**
+ * SettlementCsvGateway Gateway
+ *
+ * @author EA
+ * @package PayBreak\Sdk\Gateways
+ */
+class SettlementCsvGateway extends SettlementGateway
+{
+
+}
