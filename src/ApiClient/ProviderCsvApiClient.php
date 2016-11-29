8bf42cc0449273cb8f2a99a8e950eb70d96a462b
Manny <emmanuel.adeyemi@paybreak.com>
2016-11-29 11:55:49 +0000

    Csv Api Client

---
+++ src/ApiClient/ProviderCsvApiClient.php
@@ -0,0 +1,59 @@
+<?php
+/*
+ * This file is part of the PayBreak/basket package.
+ *
+ * (c) PayBreak <dev@paybreak.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace PayBreak\Sdk\ApiClient;
+
+use Psr\Http\Message\ResponseInterface;
+use WNowicki\Generic\ApiClient\AbstractApiClient;
+use WNowicki\Generic\ApiClient\ErrorResponseException;
+use WNowicki\Generic\ApiClient\WrongResponseException;
+use Psr\Log\LoggerInterface;
+
+/**
+ * Class ProviderCsvApiClient
+ *
+ * @author EA
+ * @package PayBreak\Sdk\ApiClient
+ */
+class ProviderCsvApiClient extends ProviderApiClient
+{
+    /**
+     * @author EA
+     * @param ResponseInterface $response
+     * @return array
+     * @throws WrongResponseException
+     */
+    protected function processResponse(ResponseInterface $response)
+    {
+        if ($response->getStatusCode() == 204) {
+            return null;
+        }
+
+        if (strpos($response->getHeaderLine('Content-Type'), 'text/csv') !== false) {
+            return ['csv' => $response->getBody()->getContents()] ;
+        }
+
+        throw new WrongResponseException('Response body was malformed csv', $response->getStatusCode());
+    }
+
+    /**
+     * @author EA
+     * @param string $uri
+     * @param array $body
+     * @param array $query
+     * @return array
+     * @throws ErrorResponseException
+     * @throws \Exception
+     */
+    public function post($uri, array $body = [], array $query = [])
+    {
+        return $this->send((new Request('POST', $uri . '.csv')), $body, $query);
+    }
+}
