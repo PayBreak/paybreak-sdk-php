67ba376dcb0108d7711e8b37bea53c0f64a644d6
Gergely Kreko <gregory.kreko@paybreak.com>
2018-08-30 09:46:21 +0100

    Add getLeadScore

---
+++ src/Gateways/CustomerIntelligenceGateway.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace PayBreak\Sdk\Gateways;
+
+use PayBreak\Sdk\Entities\InstallationEntity;
+
+/**
+ * Customer Intelligence Gateway
+ *
+ * @author GK
+ */
+class CustomerIntelligenceGateway extends AbstractGateway
+{
+    /**
+     * @author GK
+     * @param InstallationEntity $installationEntity
+     * @param $token
+     * @return array
+     * @throws \WNowicki\Generic\Exception
+     */
+    public function getLeadScore(InstallationEntity $installationEntity, $body, $token)
+    {
+        return $this->postDocument(
+            '/v4/installations/' . $installationEntity->getId() . '/lead-score',
+            $body,
+            $token,
+            'LeadScore'
+        );
+    }
+}
