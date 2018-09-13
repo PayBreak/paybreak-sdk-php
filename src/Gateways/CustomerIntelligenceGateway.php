de565173073b3c52d7d6564b33213f8230238612
Gergely Kreko <gregory.kreko@paybreak.com>
2018-09-13 11:20:15 +0100

    Change parameter order

--- src/Gateways/CustomerIntelligenceGateway.php
+++ src/Gateways/CustomerIntelligenceGateway.php
@@ -21,12 +21,12 @@ class CustomerIntelligenceGateway extends AbstractGateway
      * @author GK
      * @param string $installation
      * @param string $token
-     * @param int $limit
      * @param int $offset
+     * @param int $limit
      * @return array
      * @throws \WNowicki\Generic\Exception
      */
-    public function getCustomerIntelligence($installation, $token, $limit = 20, $offset = 0)
+    public function getCustomerIntelligence($installation, $token, $offset = 0, $limit = 20)
     {
         return $this->fetchDocument(
             '/v4/installations/' . $installation . '/lead-score?offset=' . $offset . '&limit=' . $limit,
